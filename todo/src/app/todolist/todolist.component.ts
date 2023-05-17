3680eb3be63157b8878ad1826a7dfb2ed83ef01d
Jos├® Antonio Gallo Junior <gallojunior@gmail.com>
2023-05-17 19:31:14 -0300

    Incluido o uso do localstorage

--- todo/src/app/todolist/todolist.component.ts
+++ todo/src/app/todolist/todolist.component.ts
@@ -9,20 +9,31 @@ import { NgForm } from '@angular/forms';
 export class TodolistComponent {
   taskArray = [{ taskName: 'Adicionar tarefas', isCompleted: false }];
 
+  ngOnInit(){
+    this.taskArray = JSON.parse(localStorage.getItem('taskData')!);
+  }
+
   onSubmit(form: NgForm){
    this.taskArray.push({
       taskName: form.controls['task'].value,
       isCompleted: false
     });
+    this.onSave();
     form.reset();
   }
 
   onDelete(index: number){
     this.taskArray.splice(index, 1);
+    this.onSave();
   }
 
   onCheck(index: number){
     this.taskArray[index].isCompleted = !this.taskArray[index].isCompleted;
+    this.onSave();
+  }
+
+  onSave(){
+    localStorage.setItem('taskData', JSON.stringify(this.taskArray));
   }
 
 }
